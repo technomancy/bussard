326db1a78bdcccf496c15bb647e53b27b3fdb811
Phil Hagelberg <phil@hagelb.org>
2015-09-04 22:46:05 +0700

    Controls/commands as a table.

--- default_config.lua
+++ default_config.lua
@@ -1,7 +1,12 @@
-ship.controls["up"] = ship.actions.forward
-ship.controls["left"] = ship.actions.left
-ship.controls["right"] = ship.actions.right
-ship.commands["tab"] = ship.actions.next_target
-ship.commands["return"] = ship.actions.connect
-ship.commands["escape"] = game.quit
-ship.commands["p"] = function() game.paused = (not game.paused) end
+ship.controls = {
+   up = ship.actions.forward,
+   left = ship.actions.left,
+   right = ship.actions.right
+}
+
+ship.commands = {
+   tab = ship.actions.next_target,
+   ["return"] = ship.actions.connect,
+   escape = game.quit,
+   p = function() game.paused = (not game.paused) end
+}
