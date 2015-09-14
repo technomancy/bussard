31d95b9024de525f91f8d62368ee97c577358d1b
Phil Hagelberg <phil@hagelb.org>
2015-09-14 14:39:46 +0700

    Fix some bugs with cargo buy/sell, better messages

--- cargo.lua
+++ cargo.lua
@@ -15,8 +15,17 @@ end
 local transfer = function(station, ship, direction, good, amount)
    assert(station.prices[good], station.name .. " does not trade in " .. good)
    local price = get_price(good, amount, station.prices, direction)
-   if(ship.credits >= price and space_for(amount, ship, direction) and
-      in_stock(station, ship, good, amount, direction)) then
+   if(ship.credits < price and direction == "buy") then
+      return false, "Don't have " .. price .. " credits."
+   elseif(not space_for(amount, ship, direction)) then
+      return false, "No space for " .. amount .. " of " .. good .. "."
+   elseif(not in_stock(station, ship, good, amount, direction)) then
+      if(direction == "buy") then
+         return false, "Sufficient " .. good .. " is not in stock."
+      else
+         return false, "You don't have enough " .. good .. "."
+      end
+   else
       if(direction == "sell") then
          station.cargo[good] = station.cargo[good] + amount
          ship:move_cargo(good, -amount)
