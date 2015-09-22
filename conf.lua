993f52cf57f5ced4851b5c099e57202484a4f439
Phil Hagelberg <phil@hagelb.org>
2015-09-22 20:19:23 +0700

    Avoid loading certain modules.

--- conf.lua
+++ conf.lua
@@ -1,9 +1,19 @@
 love.conf = function(t)
    t.title = "Bussard"
    t.version = "0.9.1"
-   t.window.width = 1400
-   t.window.height = 800
-   -- t.window.width = 615
-   -- t.window.height = 500
-   -- t.window.fullscreen = true
+
+   if(os.getenv("FULLSCREEN")) then
+      t.window.fullscreen = true
+      t.window.fullscreentype = "desktop"
+   else
+      -- for screenshots
+      -- t.window.width = 615
+      -- t.window.height = 500
+      t.window.width = 1400
+      t.window.height = 800
+   end
+
+   t.modules.mouse = false
+   t.modules.joystick = false
+   t.modules.physics = false
 end
