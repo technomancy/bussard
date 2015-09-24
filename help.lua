954b34e04a8ca08ffbd786a6010877466cde8420
Phil Hagelberg <phil@hagelb.org>
2015-09-24 14:58:36 +0700

    Allow listing of manual pages.

--- help.lua
+++ help.lua
@@ -1,15 +1,29 @@
-local utils = require("utils")
+local lume = require("lume")
 
-local pages = {ual = "manual.md",
-               keycodes = "doc/keycodes.md",
-               laser = "doc/laser.md"
+local pages = {
+   manual = "manual.md",
+   keycodes = "doc/keycodes.md",
+   laser = "doc/laser.md"
 }
+
+local list = function()
+   return "Manual pages:\n* " .. table.concat(lume.keys(pages), "\n* ")
+end
 
 return {
    -- need to have a string message so just `help' works vs help()
    message = "Type `man()` to view your ship's operating manual.",
-   man = function(page)
-      page = pages[page] or "manual.md"
-      return love.filesystem.read(page)
+   man = function(page_name)
+      if(page_name == "list") then
+         return list()
+      elseif(pages[page_name]) then
+         local p = love.filesystem.read(pages[page_name])
+         return p
+      elseif(page_name) then
+         return "Page not found.\n" .. list()
+      else
+         local p = love.filesystem.read("manual.md")
+         return p
+      end
    end
 }
