0293b2a0de2d0f1ad8a05364bbe99781189e4dd8
Phil Hagelberg <phil@hagelb.org>
2015-10-06 21:30:04 +0700

    Fix typo.

--- ship/hud.lua
+++ ship/hud.lua
@@ -67,6 +67,7 @@ local trajectory = function(ship, bodies, steps, step_size, color1, color2)
    end
 
    love.graphics.setLineWidth(5)
+   -- we end up calculating this twice; could get a perf boost by caching
    for i=0, steps do
       if i % 10 == 0 then
          love.graphics.setColor(color1)
