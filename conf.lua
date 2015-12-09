702becb083ae9838f267fe6226b762c3ffd3b14e
Phil Hagelberg <technomancy@gmail.com>
2015-12-08 20:57:50 -0800

    Set resolution in environment variable.

--- conf.lua
+++ conf.lua
@@ -3,8 +3,9 @@ love.conf = function(t)
 
    if(os.getenv("SCREENSHOT")) then
       t.window.width, t.window.height = 615, 500
-   elseif(os.getenv("USER") == "phil" or os.getenv("NO_FULLSCREEN")) then
-      t.window.width, t.window.height = 1400, 800
+   elseif(os.getenv("BUSSARD_RES")) then
+      local x, y = os.getenv("BUSSARD_RES"):match("(.+)x(.+)")
+      t.window.width,t.window.height = tonumber(x or "1400"),tonumber(y or "800")
    else
       t.window.fullscreen, t.window.fullscreentype = true, "desktop"
    end
