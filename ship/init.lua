9ff669dd5368aa7233a7c329ae1de5d64998ff41
Phil Hagelberg <phil@hagelb.org>
2016-01-25 17:06:54 +0700

    Don't spawn right in the middle of a portal.
    Fixes #86.

--- ship/init.lua
+++ ship/init.lua
@@ -165,7 +165,7 @@ local ship = {
 
          local portal = lume.match(ship.bodies, function(b) return b.portal == from end)
          if(portal) then
-            ship.x, ship.y = portal.x, portal.y
+            ship.x, ship.y = portal.x + 100, portal.y + 100
             ship.dx, ship.dy = portal.dx, portal.dy
          else
             ship.x, ship.y = math.random(30000) + 10000, math.random(30000) + 10000
