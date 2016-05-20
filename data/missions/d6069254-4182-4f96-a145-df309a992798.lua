bd8eda1a5c850aef146da8dd38e17ff091946b40
Phil Hagelberg <phil@hagelb.org>
2016-05-20 18:43:46 +0700

    Rename initial Nari message threads.

--- data/missions/d6069254-4182-4f96-a145-df309a992798.lua
+++ data/missions/d6069254-4182-4f96-a145-df309a992798.lua
@@ -23,35 +23,35 @@ return {
       if(not m.pickup and target == "Tana Prime") then
          m.pickup = 0
       elseif(target == "Interportal: Sol" and not ship.events.try_interportal) then
-         mail.deliver_msg(ship, "nari07.msg")
+         mail.deliver_msg(ship, "nari-a-07.msg")
          mission.record_event(ship, "try_interportal")
       elseif(target == "Apkabar Station" and ship.events.try_interportal
              and not ship.events.background_check) then
-         mail.deliver_msg(ship, "nari08.msg")
+         mail.deliver_msg(ship, "nari-a-08.msg")
          mission.record_event(ship, "background_check")
-      elseif(ship.events.invite_nari and not ship.mail_delivered["nari05"] and
+      elseif(ship.events.invite_nari and not ship.mail_delivered["nari-a-05"] and
              target == "Tana Prime") then
-         mail.deliver_msg(ship, "nari05.msg")
+         mail.deliver_msg(ship, "nari-a-05.msg")
       end
    end,
 
    accept_function = function(ship)
-      mail.deliver_msg(ship, "nari01.msg")
+      mail.deliver_msg(ship, "nari-a-01.msg")
    end,
 
    update = function(ship, dt)
       local m = ship.active_missions["d6069254-4182-4f96-a145-df309a992798"]
       if(not m or not m.pickup) then return end
       m.pickup = m.pickup + dt
-      if(m.pickup > 12 and not ship.mail_delivered["nari02"]) then
-         mail.deliver_msg(ship, "nari02.msg")
-      elseif(m.pickup > 32 and not ship.mail_delivered["nari03"]) then
-         mail.deliver_msg(ship, "nari03.msg")
+      if(m.pickup > 12 and not ship.mail_delivered["nari-a-02"]) then
+         mail.deliver_msg(ship, "nari-a-02.msg")
+      elseif(m.pickup > 32 and not ship.mail_delivered["nari-a-03"]) then
+         mail.deliver_msg(ship, "nari-a-03.msg")
       end
    end,
 
    success_function = function(ship)
-      mail.deliver_msg(ship, "nari09.msg")
+      mail.deliver_msg(ship, "nari-a-09.msg")
    end,
 }
 
