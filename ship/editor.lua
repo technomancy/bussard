8630051b37773782ff1f78ce9f25b807fe3c7448
Phil Hagelberg <phil@hagelb.org>
2016-08-13 22:41:26 +0700

    Reintroduce missing arg to save.

--- ship/editor.lua
+++ ship/editor.lua
@@ -391,7 +391,7 @@ end
 
 local backward_word = lume.fn(forward_word, -1)
 
-local save = function(this_fs)
+local save = function(this_fs, this_path)
    local target = this_fs or b and b.fs
    local this_b = (b and b.path == "minibuffer") and behind_minibuffer or b
    if(not target or not this_b.needs_save) then return end
