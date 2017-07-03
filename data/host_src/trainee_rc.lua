d2a3daa75900885080378c2438dd371ba59bf643
Phil Hagelberg <phil@hagelb.org>
2017-07-02 21:26:46 -0700

    Move trainee motd into smashrc.

--- data/host_src/trainee_rc.lua
+++ data/host_src/trainee_rc.lua
@@ -1,3 +1,9 @@
 -- TODO: this shouldn't be an RPC!
 
 record_event("trainee01")
+
+print("You made it to the terminal, nice work. You can log out of this session by")
+print("running `logout', and then log out of the rover with the same.\n")
+
+print("Once your application is reviewed, you will receive an email from PTMC")
+print("recruiting with further instructions.")
